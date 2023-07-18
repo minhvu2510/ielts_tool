884b555601676e388f42efbb3eaba243c855ac7f
vu.nguyen <vunguyen@dnse.com.vn>
2023-07-18 21:18:00 +0700

    custom for ielts

--- src/be_eng/src/db.py
+++ src/be_eng/src/db.py
@@ -4,18 +4,21 @@ import sys
 # sys.path.append('../')
 from pymongo import MongoClient
 import settings
-from random import randrange,sample
+from random import randrange, sample
 
 client = MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
-DATABASE = client.mvp
-def get_document(table,query,order=None,distinct=None,page=None,limit=None,incre=-1):
+DATABASE = client.ielts
+
+
+def get_document(table, query, order=None, distinct=None, page=None, limit=None, incre=-1):
     if page:
         if not limit:
             limit = 20
         page = int(page)
         limit = int(limit)
         if order:
-            res = DATABASE[table].find(query).sort(order, incre).skip((page-1)*limit).limit(limit)
+            res = DATABASE[table].find(query).sort(
+                order, incre).skip((page-1)*limit).limit(limit)
         else:
             res = DATABASE[table].find(query).skip((page-1)*limit).limit(limit)
     else:
@@ -25,19 +28,23 @@ def get_document(table,query,order=None,distinct=None,page=None,limit=None,incre
             res = DATABASE[table].find(query)
     if distinct:
         res = res.distinct(distinct)
-        res = filter(lambda r: r != '',res)
+        res = filter(lambda r: r != '', res)
     if res:
         message = {"data": list(res)}
     else:
         message = {"data": []}
-    return  message
-def get_limit(table,query,limit):
+    return message
+
+
+def get_limit(table, query, limit):
     res = DATABASE[table].find(query).limit(limit)
     if res:
         message = {"data": list(res)}
     else:
         message = {"data": []}
-    return  message
+    return message
+
+
 def get_all(table):
     res = DATABASE[table].find()
     res = filter(lambda r: r != '', res)
@@ -45,16 +52,20 @@ def get_all(table):
         message = {"data": list(res)}
     else:
         message = {"data": []}
-    return  message
-def get_sort(table,order,incre):
-    res = DATABASE[table].find().sort(order,incre)
+    return message
+
+
+def get_sort(table, order, incre):
+    res = DATABASE[table].find().sort(order, incre)
     res = filter(lambda r: r != '', res)
     if res:
         message = {"data": list(res)}
     else:
         message = {"data": []}
     return message
-def get_records_top(table,order,incre,skip):
+
+
+def get_records_top(table, order, incre, skip):
     res = DATABASE[table].find().sort(order, incre).skip(skip)
     # res = DATABASE[table].find().sort(order, incre).limit(skip)
     res = filter(lambda r: r != '', res)
@@ -63,7 +74,9 @@ def get_records_top(table,order,incre,skip):
     else:
         message = {"data": []}
     return message
-def find_one(table,order,incre,skip):
+
+
+def find_one(table, order, incre, skip):
     res = DATABASE[table].find_one().skip(skip)
     res = filter(lambda r: r != '', res)
     if res:
@@ -71,17 +84,22 @@ def find_one(table,order,incre,skip):
     else:
         message = {"data": []}
     return message
+
+
 def insert_document(table, query, file_unique=None):
     if file_unique:
-        DATABASE[table].create_index(file_unique,unique = True)
+        DATABASE[table].create_index(file_unique, unique=True)
     try:
         DATABASE[table].insert(query)
         message = {"status": True, "message": "Insert success"}
         if "id" in query:
             message.update({"id": query["id"]})
     except:
-        message = {"status": False, "message": "{} is exists.".format(file_unique)}
+        message = {"status": False,
+                   "message": "{} is exists.".format(file_unique)}
     return message
+
+
 def update_document(table, query, query_update):
     try:
         DATABASE[table].update(query, {"$set": query_update}, multi=True)
@@ -89,6 +107,8 @@ def update_document(table, query, query_update):
     except:
         message = {"status": False, "message": "Update error"}
     return message
+
+
 def delete_document(table, query):
     try:
         DATABASE[table].remove(query)
@@ -103,35 +123,55 @@ def count_document(table, query=None):
         return DATABASE[table].find(query).count()
     else:
         return DATABASE[table].count()
+
+
 def show_allcolection():
     allcolec = []
-    for i in DATABASE.collection_names():
-        allcolec.append(i)
-    return allcolec
-def get_all_record(table,query):
+    all_collections = DATABASE.collection_names()
+    print(all_collections)
+    topic_collections = [
+        col for col in all_collections if col.startswith('topic')]
+    return list(topic_collections)
+
+
+def get_all_record(table, query):
     res = DATABASE[table].find(query)
     return list(res)
 
+
+def get_top_records(table, query):
+    res = DATABASE[table].find(query)
+    return list(res)
+
+
 def get_all_topic():
-    topics = get_all_record('ignor_topics', {})
-    ignore = ['none', 'user', 'ig_topics', 'igr_topics', 'igno_topics', 'note','tempt']
-    result = [topic for topic in topics if topic['topic'] not in ignore]
+    query = {}
+    topics = get_all_record('ignor_topics', query)
+    ignore = ['none', 'user', 'ig_topics',
+              'igr_topics', 'igno_topics', 'note', 'tempt']
+    result = [topic for topic in topics if topic['topic'].startswith('Topic')]
     return result
+
 
 def get_difficult():
     priority = get_all_record('ignor_topics', {'favorite': 1})
     list_name_priority = list(map(lambda x: x['topic'], priority))
     print(list_name_priority, type(list_name_priority))
     length_tb = count_document('ignor_topics')
-    hight_lv = sample(range(length_tb - 14, length_tb-4), 6)   # lấy random 5 topic trong 10 topic
+    # lấy random 5 topic trong 10 topic
+    hight_lv = sample(range(length_tb - 14, length_tb-4), 6)
     print(hight_lv)
-    list_topics_hightlv = get_document('ignor_topics', {'order': {'$in': hight_lv}}).get("data")
-    list_name_topics_highlv = list(map(lambda x: x['topic'], list_topics_hightlv))
+    list_topics_hightlv = get_document(
+        'ignor_topics', {'order': {'$in': hight_lv}}).get("data")
+    list_name_topics_highlv = list(
+        map(lambda x: x['topic'], list_topics_hightlv))
     print(list_name_topics_highlv)
     medi_lv = sample(range(1, length_tb-14), 11)
     print(medi_lv)
-    list_topics_medilv = get_document('ignor_topics', {'order': {'$in': medi_lv}}).get("data")
-    list_name_topics_medilv = list(map(lambda x: x['topic'], list_topics_medilv))
+    list_topics_medilv = get_document(
+        'ignor_topics', {'order': {'$in': medi_lv}}).get("data")
+    list_name_topics_medilv = list(
+        map(lambda x: x['topic'], list_topics_medilv))
     print(list_name_topics_medilv)
     hight_topics = list(set(list_name_priority + list_name_topics_highlv))
     result = []
@@ -149,6 +189,8 @@ def get_difficult():
         print(list_ext_id)
         result.extend(list_ext_id[0:4])
     return result
+
+
 def write_tempt():
     try:
         result = get_difficult()
@@ -162,6 +204,7 @@ def write_tempt():
         return True
     except:
         return False
+
 
 if __name__ == '__main__':
     print(get_difficult())
--- src/be_eng/src/main.py
+++ src/be_eng/src/main.py
@@ -14,15 +14,21 @@ import random
 from datetime import date, timedelta, datetime
 from bson import ObjectId
 from bson.json_util import dumps as dumps_json
-from flask import (Flask, request, Blueprint, url_for,
-                   send_from_directory, Response,
-                   render_template, redirect, session, g, jsonify)
-reload(sys)
-sys.setdefaultencoding('utf-8')
-# sys.path.append('../')
+from flask import Flask, request, Blueprint, url_for, \
+    send_from_directory, Response, \
+    render_template, redirect, session, g, jsonify
+import sys
+import requests
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app)
+
+if sys.version_info.major < 3:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+
 class InvalidUsage(Exception):
     status_code = 400
 
@@ -37,11 +43,15 @@ class InvalidUsage(Exception):
         rv = dict(self.payload or ())
         rv['message'] = self.message
         return rv
+
+
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
+
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -63,20 +73,25 @@ def login_required(f):
             raise InvalidUsage(message, status_code=406)
         return f(*args, **kwargs)
     return decorated_function
+
+
 @app.route('/login', methods=["POST"])
 def login():
     if request.method == 'POST':
-        data= json.loads(request.data)
-        mess = utils.check_require(['name','pass'],data)
+        data = json.loads(request.data)
+        mess = utils.check_require(['name', 'pass'], data)
         if mess['status']:
             name = data.get('name')
             passw = data.get('pass')
-            user = db.get_document('user', {'name': name,'pass':passw}).get("data")
+            user = db.get_document(
+                'user', {'name': name, 'pass': passw}).get("data")
             if user:
                 token = str(uuid4())
-                message = db.update_document("user", {"name": name}, {"token": token})
+                message = db.update_document(
+                    "user", {"name": name}, {"token": token})
                 date_old = datetime.strptime(user[0]['dateLog'], '%Y-%m-%d')
-                date_new = datetime.strptime(str(datetime.now().date()), '%Y-%m-%d')
+                date_new = datetime.strptime(
+                    str(datetime.now().date()), '%Y-%m-%d')
                 day = (date_new - date_old).days
                 if (day > 1):
                     info = {
@@ -91,39 +106,48 @@ def login():
                     }
                 db.update_document('user', {'name': user[0]['name']}, info)
                 if message:
-                    messange = {'status': True,'token':token}
+                    messange = {'status': True, 'token': token}
                 else:
                     messange = {'status': False}
             else:
                 messange = {'status': False}
     return dumps_json(messange)
+
+
 @app.route('/', methods=["GET"])
 def welcome():
     messange = {'status': True, 'mess': 'Welcome'}
     return dumps_json(messange)
+
+
 @app.route('/user', methods=["GET"])
 def getInfo():
     if request.method == "GET":
         token = request.headers.get('Authorization')
         user = db.get_document('user', {'token': token}).get("data")
         if user:
-            return dumps_json({'status': True,'name': user[0]['name'],'innings':user[0]['innings'],'incomplete':user[0]['NotDone']})
+            return dumps_json({'status': True, 'name': user[0]['name'], 'innings': user[0]['innings'], 'incomplete': user[0]['NotDone']})
     return dumps_json({'status': False})
-@app.route('/test', methods=["POST","GET"])
+
+
+@app.route('/test', methods=["POST", "GET"])
 # @login_required
 def test():
     if request.method == "GET":
-      pass
+        pass
     else:
         data = json.loads(request.data)
     return 'minhvu'
-@app.route('/preview', methods=["POST","GET","PUT","DELETE"])
+
+
+@app.route('/preview', methods=["POST", "GET", "PUT", "DELETE"])
 def topic():
     if request.method == "GET":
         table = request.args.get('table')
-        table = str(table).lower().replace(' ','_')
+        table = str(table).lower().replace(' ', '_')
         level = int(request.args.get('level'))
-        list_ext_id = db.get_document(table, {'level': {'$gt':level}}).get("data")
+        list_ext_id = db.get_document(
+            table, {'level': {'$gt': level}}).get("data")
         conunt = len(list_ext_id)
 
         info_response = {
@@ -135,15 +159,47 @@ def topic():
         data = json.loads(request.data)
         table = data.get('table')
         table = str(table).lower().replace(' ', '_')
+        url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + \
+            data.get('key')
+        payload = {}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        print("response.status_code", response.status_code)
+        myMeanings = []
+        audio = ""
+        if (response.status_code != 404):
+            resultParsed = eval(response.content)
+            phonetics = resultParsed[0].get("phonetics")
+            phoneticsUs = phonetics[-1]
+            print(phoneticsUs)
+            audio = phoneticsUs.get('audio')
+            print(audio)
+            meanings = resultParsed[0].get("meanings")
+            for mean in meanings:
+                print(mean.get("example"))
+                newDict = {
+                    "type": mean.get("partOfSpeech"),
+                    "synonyms": mean.get("synonyms"),
+                    "antonyms": mean.get("antonyms"),
+                    "definition": mean.get("definitions")
+                }
+                myMeanings.append(newDict)
+            print(myMeanings)
         info = {
             "key": data.get('key'),
             "value": data.get('value'),
-            "level": int(data.get('level'))
+            "level": int(data.get('level')),
+            "phonetic": resultParsed[0].get("phonetic"),
+            "audio": audio,
+            "mean": myMeanings,
+            "v2":  data.get('v2'),
+            "v3": data.get('v3')
         }
-        if(len(str(data.get('key')))<1 or len(str(data.get('value')))<1):
-            info_response = {"status": False, "message": "key or value is null"}
+        if (len(str(data.get('key'))) < 1 or len(str(data.get('value'))) < 1):
+            info_response = {"status": False,
+                             "message": "key or value is null"}
         else:
-            info_response = db.insert_document(table,info,"key")
+            info_response = db.insert_document(table, info, "key")
     if request.method == "PUT":
         data = json.loads(request.data)
         table = data.get('table')
@@ -155,9 +211,11 @@ def topic():
             "level": int(data.get('level'))
         }
         if (len(str(data.get('key'))) < 1 or len(str(data.get('value'))) < 1):
-            info_response = {"status": False, "message": "key or value is null"}
+            info_response = {"status": False,
+                             "message": "key or value is null"}
         else:
-            info_response = db.update_document(table,{'_id':ObjectId(table1)},info)
+            info_response = db.update_document(
+                table, {'_id': ObjectId(table1)}, info)
     if request.method == "DELETE":
         data = json.loads(request.data)
         table = data.get('table')
@@ -165,7 +223,9 @@ def topic():
         table = str(table).lower().replace(' ', '_')
         info_response = db.delete_document(table, {'_id': ObjectId(table1)})
     return dumps_json(info_response)
-@app.route('/note', methods=["POST","GET"])
+
+
+@app.route('/note', methods=["POST", "GET"])
 def note():
     if request.method == "GET":
         list_ext_id = db.get_all('note').get("data")
@@ -183,25 +243,29 @@ def note():
             }
         else:
             info = {
-                'note':data.get('note'),
-                'level':data.get('level')
+                'note': data.get('note'),
+                'level': data.get('level')
             }
             info_response = db.insert_document('note', info, "note")
         return dumps_json(info_response)
-    return dumps_json({'status':False})
-@app.route('/showexample', methods=["POST","GET","PUT","DELETE"])
+    return dumps_json({'status': False})
+
+
+@app.route('/showexample', methods=["POST", "GET", "PUT", "DELETE"])
 def showexample():
-    k=[]
-    list_data=[]
+    k = []
+    list_data = []
     if request.method == "GET":
-        k=db.show_allcolection()
+        k = db.show_allcolection()
         for i in k:
             len_col = db.count_document(i)
             ramdum = randrange(len_col)
-            if(len_col < 13):
-                list_ext_id = db.get_document(i, {'level': {'$gt': 5}}).get("data")
+            if (len_col < 13):
+                list_ext_id = db.get_document(
+                    i, {'level': {'$gt': 5}}).get("data")
             else:
-                list_ext_id = db.get_records_top(i,"level",1,(len_col - 12)).get("data")
+                list_ext_id = db.get_records_top(
+                    i, "level", 1, (len_col - 12)).get("data")
             dict = {}
             dict['table'] = i
             dict['data'] = list_ext_id
@@ -211,10 +275,12 @@ def showexample():
         key = data.get('key')
         table = data.get('table')
         table = str(table).lower().replace(' ', '_')
-        info_response = db.update_document(table,{'key':key},{'level': 15})
+        info_response = db.update_document(table, {'key': key}, {'level': 15})
         return dumps_json(info_response)
     return dumps_json(list_data)
-@app.route('/extension', methods=["POST","GET","PUT","DELETE"])
+
+
+@app.route('/extension', methods=["POST", "GET", "PUT", "DELETE"])
 @login_required
 def extension():
     if request.method == "GET":
@@ -225,20 +291,23 @@ def extension():
         vague = 0
         forget = 0
         for i in k:
-            if(i != 'extention'):
+            if (i != 'extention'):
                 total += db.count_document(i)
-                expert += db.count_document(i,{'level':{'$lt': 10}})
-                vague += db.count_document(i,{'$and' :[{'level':{'$gt': 9}},{'level':{'$lt': 13}}]})
-                forget += db.count_document(i,{'level':{'$gt': 12}})
+                expert += db.count_document(i, {'level': {'$lt': 10}})
+                vague += db.count_document(
+                    i, {'$and': [{'level': {'$gt': 9}}, {'level': {'$lt': 13}}]})
+                forget += db.count_document(i, {'level': {'$gt': 12}})
         # list_ext_id = db.get_records_top('extention', "level", 1, 1).get("data")
-        list_ext_id = db.get_document('extention', {'level':{'$gt':9}}).get("data")
-        info_response = {'total':total,'expert':expert,'vague':vague,'forget':forget,'data':list_ext_id}
+        list_ext_id = db.get_document(
+            'extention', {'level': {'$gt': 9}}).get("data")
+        info_response = {'total': total, 'expert': expert,
+                         'vague': vague, 'forget': forget, 'data': list_ext_id}
     if request.method == "POST":
         data = json.loads(request.data)
         k = db.show_allcolection()
         ramdum = randrange(len(k))
-        if(k[ramdum] == 'extention'):
-            ramdum = ramdum +1
+        if (k[ramdum] == 'extention'):
+            ramdum = ramdum + 1
         info = {
             "key": data.get('key'),
             "value": data.get('value'),
@@ -246,12 +315,15 @@ def extension():
             "creat_day": data.get('creat_day')
         }
         if (len(str(data.get('key'))) < 1 or len(str(data.get('value'))) < 1):
-            info_response = {"status": False, "message": "key or value is null"}
+            info_response = {"status": False,
+                             "message": "key or value is null"}
         else:
             info_response12 = db.insert_document(k[ramdum], info, "key")
             info_response = db.insert_document('extention', info, "key")
     return dumps_json(info_response)
-@app.route('/confuse', methods=["POST","GET"])
+
+
+@app.route('/confuse', methods=["POST", "GET"])
 def confuse():
     if request.method == 'GET':
         a = request.args.get('level')
@@ -259,7 +331,8 @@ def confuse():
         list_result = []
         for i in k:
             if (i != 'user') and (i != 'note'):
-                list_ext_id = db.get_limit(i, {'level': {'$gt': int(a)}},44).get("data")
+                list_ext_id = db.get_limit(
+                    i, {'level': {'$gt': int(a)}}, 44).get("data")
                 if list_ext_id:
                     for j in list_ext_id:
                         list_result.append(j)
@@ -269,16 +342,26 @@ def confuse():
         }
         return dumps_json(info_response)
     else:
-        return dumps_json({'status':False})
-@app.route('/topics', methods=["POST","GET","PUT","DELETE"])
+        return dumps_json({'status': False})
+
+
+@app.route('/topics', methods=["POST", "GET", "PUT", "DELETE"])
 def topics():
     if request.method == 'GET':
         topics = db.get_all_topic()
         newlist = sorted(topics, key=lambda k: k['order'])
         for it in newlist:
-            it['word'] = []
-            it['link'] = img_topic.img[random.randint(0, len(img_topic.img) - 1)]
-        # newl = [it.update( {"word":[]}) for it in newlist]
+            print(it)
+            topic = it.get('topic').lower()
+            len_col = db.count_document(topic)
+            dataIntable = db.get_records_top(
+                topic, "level", 1, (len_col - 12) if len_col > 12 else 0).get('data')
+            fields = ['key', 'audio']
+            result = list(
+                map(lambda d: {field: d[field] for field in fields}, dataIntable))
+            it['word'] = result
+            print(it['word'])
+
         info_response = {
             "status": True,
             "data": newlist
@@ -292,7 +375,8 @@ def topics():
             "topic": data.get('topic'),
             "level": int(data.get('level')),
             "favorite": int(data.get('favorite')),
-            "order": db.count_document('ignor_topics', {})
+            "order": db.count_document('ignor_topics', {}),
+            "avatar": data.get('avatar'),
         }
         info_response = db.insert_document('ignor_topics', info, 'topic')
         print(info_response)
@@ -305,7 +389,8 @@ def topics():
             "topic": data.get('topic'),
             "level": int(data.get('level')),
             "favorite": int(data.get('favorite')),
-            "order": int(data.get('order'))
+            "order": int(data.get('order')),
+            "avatar": data.get('avatar')
         }
         info_response = db.update_document('ignor_topics', {'idi': idi}, info)
         return dumps_json(info_response)
@@ -319,8 +404,10 @@ def topics():
         info_response = db.delete_document('ignor_topics', {'idi': idi})
         return dumps_json(info_response)
     else:
-        return dumps_json({'status':False})
-@app.route('/difficult', methods=["POST","GET","PUT","DELETE"])
+        return dumps_json({'status': False})
+
+
+@app.route('/difficult', methods=["POST", "GET", "PUT", "DELETE"])
 def difficlut():
     if request.method == 'GET':
         # db.write_tempt()
@@ -336,15 +423,17 @@ def difficlut():
         data = json.loads(request.data)
         # 'idi': 'expand01', 'topic': 'expand01', 'level': 12, 'favorite': 0
         confirm = data.get('confirm'),
-        db.delete_document('tempt',{})
+        db.delete_document('tempt', {})
         db.write_tempt()
         info_response = {'status': True}
         return dumps_json(info_response)
     else:
-        return dumps_json({'status':False})
+        return dumps_json({'status': False})
+
+
 if __name__ == '__main__':
-  try:
-    port = int(sys.argv[1])
-  except (TypeError, IndexError):
-    port = 8100
-  app.run(debug=True, host='0.0.0.0', port=port)
+    try:
+        port = int(sys.argv[1])
+    except (TypeError, IndexError):
+        port = 8100
+    app.run(debug=True, host='0.0.0.0', port=port)
